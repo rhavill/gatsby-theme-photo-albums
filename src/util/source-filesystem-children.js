aa7a8dbcca5a043aad4579238fb8953284d27a67
Ricardo Havill <rhavill2002@yahoo.com>
2019-09-29 01:03:06 -0400

    Fixed bug: when making a list of child directories, only include immediate descendants.

--- src/util/source-filesystem-children.js
+++ src/util/source-filesystem-children.js
@@ -1,15 +1,25 @@
 import startsWith from 'ramda/src/startsWith'
 
-export const getChildren = (path, data) => {
+const getChildren = (path, data) => {
+  const currentPath = path.replace(/\/$/, '')
+  const folders = [], files = []
+  data.allDirectory.edges.forEach(edge => {
+    // Add leading slash to filesystem path to be consistent with current location path
+    const fsPath = '/' + edge.node.relativePath
+    console.log('FS fsPath', level(fsPath), fsPath, 'browser currentPath', level(currentPath), currentPath)
+    if (currentPath !== fsPath && startsWith(currentPath, fsPath)
+        && level(currentPath) + 1 === level(fsPath) && fsPath !== '/') {
+      folders.push(fsPath)
+    }
+  })
   const children = {
-    folders: [
-      data.allDirectory.edges.filter(edge => path !== edge.node.relativePath 
-        && startsWith(path, edge.node.relativePath))
-    ],
+    folders,
     files: []
   };
   console.log('getChildren path', path, 'data', data, 'children', children);
   return children;
 }
 
-// export default getChildren;
+const level = path => path.split('/').length
+
+export default getChildren;
