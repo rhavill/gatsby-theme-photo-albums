a0d7a194f277a55aaf16840c5485c875899f0c11
Ricardo Havill <rhavill2002@yahoo.com>
2019-10-18 00:48:33 -0400

    More test data refactoring.

--- src/util/source-filesystem-child-paths.test.js
+++ src/util/source-filesystem-child-paths.test.js
@@ -1,19 +1,15 @@
+import map from 'ramda/src/map'
+import prop from 'ramda/src/prop'
 import getChildPaths from './source-filesystem-child-paths'
+import folderData from '../test-data/source-filesystem-folder-data'
+
+const folders = map(prop('relativePath'), folderData.folders.nodes)
 
 describe("source-filesystem-child-paths", () => {  
 
-  const directoryData =  [
-    '',
-    'level-one',
-    'another-level-one',
-    'yet-another-level-one',
-    'level-one/level-two',
-    'level-one/another-level-two'
-  ]
-
   it("returns child directories of current path", () => {
     const path = '/level-one'
-    const children = getChildPaths(path, directoryData)
+    const children = getChildPaths(path, folders)
     const expectedFolders =  [
       '/level-one/level-two',
       '/level-one/another-level-two'
@@ -23,7 +19,7 @@ describe("source-filesystem-child-paths", () => {
 
   it("returns all top-level directories of root path", () => {
     const path = ''
-    const children = getChildPaths(path, directoryData)
+    const children = getChildPaths(path, folders)
     const expectedFolders = [
       '/level-one',
       '/another-level-one',
