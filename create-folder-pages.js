b8c327af2b666b01440bdc943f4eaf94b39573c3
Ricardo Havill <rhavill2002@yahoo.com>
2019-10-20 14:31:40 -0400

    Changed createFolderPages function to make it more readable.

--- create-folder-pages.js
+++ create-folder-pages.js
@@ -1,18 +1,31 @@
 const path = require(`path`)
+const add = require('ramda/src/add')
+const and = require('ramda/src/and')
+const compose = require('ramda/src/compose')
+const concat = require('ramda/src/concat')
+const equals = require('ramda/src/equals')
+const not = require('ramda/src/not')
 const emitter = require('./src/util/event-emitter')
 const {getPagerData} = require('./src/util/source-filesystem-pager-data')
+
+const pathToUrl = concat('/')
+const isFirstPage = pageIndex => equals(0, pageIndex)
+const isNotFirstPage = compose(not, isFirstPage)
+const isRootUrl = equals('/')
+const isNotRootUrl = compose(not, isRootUrl)
+const pageIndexToPageNumber = add(1)
 
 const createFolderPages = (photosPerPage, createPage, files, folders) => {
   folders.forEach(folder => {
     const pagerData = getPagerData(folder, files, photosPerPage)
     const regexFilter = '/^' + folder + (folder ? '\/' : '') + '[^/]+$/'
     pagerData.forEach((pagerData, i) => {
-      let url = '/' + folder
-      if (i > 0 && url !== '/') {
-        url += '/'
+      let url = pathToUrl(folder)
+      if (and(isNotFirstPage(i), isNotRootUrl(url))) {
+        url = concat(url, '/')
       }
-      if (i > 0) {
-        url += i + 1
+      if (isNotFirstPage(i)) {
+        url = concat(url, `${pageIndexToPageNumber(i)}`)
       }
       if (url === '/') {
         emitter.emit('indexContext', {
@@ -20,7 +33,6 @@ const createFolderPages = (photosPerPage, createPage, files, folders) => {
           regexFilter
         })
       }
-      
       createPage({
         path: url,
         component: path.resolve(`./src/pages/index.js`),
