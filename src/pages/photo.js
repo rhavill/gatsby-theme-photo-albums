a0472e1d3e52a4af7e6d78922a7f59849794494c
Ricardo Havill <rhavill2002@yahoo.com>
2019-10-22 20:57:00 -0400

    Added more image sized to the photo page.

--- src/pages/photo.js
+++ src/pages/photo.js
@@ -23,7 +23,7 @@ export const query = graphql`
   query photoQuery($relativePath: String!) {
     photo: file(relativePath: {eq: $relativePath}) {
       childImageSharp {
-        fluid(maxWidth: 512, srcSetBreakpoints: [512, 614], fit: COVER) {
+        fluid(maxWidth: 512, srcSetBreakpoints: [512, 614, 819, 1093], fit: COVER) {
           ...GatsbyImageSharpFluid
         }
       }
