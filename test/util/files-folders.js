70cae639131a781efbc4d0f661ce6b31e5ff14c6
Ricardo Havill <rhavill@protonmail.com>
2020-01-21 17:35:05 -0400

    Added some tests for files-folders and got rid of some unused tests.

--- test/util/files-folders.js
+++ test/util/files-folders.js
@@ -1,13 +1,14 @@
 import map from 'ramda/src/map'
 import prop from 'ramda/src/prop'
-import {getChildPaths} from '../../src/util/files-folders'
+import {getChildPaths, getPagerData} from '../../src/util/files-folders'
+import {objectArrayToPropArray} from '../../src/util/ramda-utils'
 import folderData from '../../test-data/source-filesystem-folder-data'
 import fileData from '../../test-data/source-filesystem-file-data'
 
 const folders = map(prop('url'), folderData.folders.nodes)
 const files = map(prop('url'), fileData.photos.nodes)
 
-describe('source-filesystem-child-paths', () => {  
+describe('files-folders getChildPaths', () => {  
 
   it('returns child directories of current path', () => {
     const path = '/base/level-one'
@@ -48,4 +49,46 @@ describe('source-filesystem-child-paths', () => {
     expect(children).toEqual(expectedFiles)
   })
 
+})
+
+describe('files-folders getPagerData', () => { 
+  const pagerFiles = [
+    '/base/sub-folder/1.jpg',
+    '/base/sub-folder/2.jpg',
+    '/base/sub-folder/3.jpg',
+    '/base/sub-folder/4.jpg',
+    '/base/sub-folder/5.jpg',
+    '/base/sub-folder/6.jpg',
+    '/base/7.jpg',
+    '/base/8.jpg',
+    '/9.jpg'
+  ] 
+
+  it('returns pager data of 3 pages of files in a directory', () => {
+    const path = '/base/sub-folder'
+    const data = getPagerData(path, pagerFiles, 2)
+    const expectedResult =  [
+      {limit: 2, skip: 0, numPages: 3, currentPage: 1},
+      {limit: 2, skip: 2, numPages: 3, currentPage: 2},
+      {limit: 2, skip: 4, numPages: 3, currentPage: 3},
+    ]
+    expect(data).toEqual(expectedResult)
+  })
+  it('returns pager data of a single file in the root directory', () => {
+    const path = '/'
+    const data = getPagerData(path, pagerFiles, 5)
+    const expectedResult =  [
+      {limit: 5, skip: 0, numPages: 1, currentPage: 1},
+    ]
+    expect(data).toEqual(expectedResult)
+  })
+  it('returns pager data of a single folder in the root directory', () => {
+    const path = ''
+    const files = []
+    const data = getPagerData(path, files, 5)
+    const expectedResult =  [
+      {limit: 5, skip: 0, numPages: 1, currentPage: 1},
+    ]
+    expect(data).toEqual(expectedResult)
+  })
 })
