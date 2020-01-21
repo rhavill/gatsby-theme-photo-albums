5d6552e38ef076f84a2b35aa44c1619e9e27d5b9
Ricardo Havill <rhavill@protonmail.com>
2020-01-21 16:40:12 -0400

    Moved ramda-utils tests to the new test directory and fixed some errors.

--- test/util/ramda-utils.test.js
+++ test/util/ramda-utils.test.js
@@ -1,4 +1,4 @@
-import {objectArrayToPropArray} from './ramda-utils'
+import {objectArrayToPropArray} from '../../src/util/ramda-utils'
 
 describe('ramda-utils', () => {
   it('extracts a single property from an array of objects', () => {
