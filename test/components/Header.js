689a3ae8a8b0136b64df836ddd49ddeda43dc06c
Ricardo Havill <rhavill@protonmail.com>
2020-01-21 23:03:30 -0400

    Moved Header component tests to the new test directory.

--- test/components/Header.js
+++ test/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import {cleanup, render} from '@testing-library/react'
-import Header from './Header'
+import Header from '../../src/components/Header'
 
 // automatically unmount and cleanup DOM after the test is finished.
 afterEach(cleanup)
