04717e13ab0069b2e2f676344f87438976f9ca14
Ricardo Havill <rhavill@protonmail.com>
2020-01-23 20:17:17 -0400

    Added custom colors for the demo site.

--- demo/src/gatsby-theme-photo-albums/theme.js
+++ demo/src/gatsby-theme-photo-albums/theme.js
@@ -3,8 +3,18 @@ import baseTheme from 'gatsby-theme-photo-albums/src/theme'
 
 export const theme = merge({}, baseTheme, {
   colors: {
-    background: '#eee',
+    text: '#fff',
+    background: '#4f4f4f',
+    primary: '#3c3c3c',
+    secondary: '#9e9e9e',
   },
+  styles: {
+    Main: {
+      '.folder': {
+        color: 'secondary'
+      }
+    }
+  }
 })
 
 export default theme
